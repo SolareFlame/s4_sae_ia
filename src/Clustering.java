0120d875cb03f2305607d7ee88e87ac8545e51fe
SolareFlame <zeldac332@gmail.com>
2025-06-04 12:48:16 +0200

    Add Clustering interface for data classification

---
+++ src/Clustering.java
@@ -0,0 +1,10 @@
+public interface Clustering {
+
+    /**
+     * Effectue un clustering sur les données fournies.
+     *
+     * @param data tableau de taille Nobjet x Ncarac représentant les objets à classer
+     * @return tableau de taille Nobjet contenant le numéro de cluster attribué à chaque objet
+     */
+    int[] assignerClusters(double[][] data);
+}

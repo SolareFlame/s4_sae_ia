74c9bf1b423e2fdd346249d670f53b0ef302df4b
SolareFlame <zeldac332@gmail.com>
2025-06-04 13:58:17 +0200

    🚀 Add Main class with sample data for clustering testing

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        double[][] data = {
+                {1.0, 2.0},
+                {1.2, 2.4},
+                {1.8, 1.9},
+                {8.0, 8.1},
+                {8.2, 8.5},
+                {100.0, 100.0}
+        };
+    }
+}
